9e7d4e74fd06fd369ccb556a1b4c9018f62d6d74
Davi <105817167+Davi220506@users.noreply.github.com>
2023-06-25 17:50:20 -0300

    Add files via upload

---
+++ pc_web/catalogo_filmes/js/script.js
@@ -0,0 +1,18 @@
+const botaof1 = document.querySelector("#botaof1");
+const botaof2 = document.querySelector("#botaof2");
+const botaof3 = document.querySelector("#botaof3");
+
+botaof1.addEventListener("click", () => {
+    let descricao1 = document.querySelector("#desc-f1");
+    descricao1.innerHTML = "O filme...";
+})
+
+botaof2.addEventListener("click", () => {
+    let descricao2 = document.querySelector("#desc-f2");
+    descricao2.innerHTML = "O filme...";
+})
+
+botaof3.addEventListener("click", () => {
+    let descricao3 = document.querySelector("#desc-f3");
+    descricao3.innerHTML = "O filme...";
+})
